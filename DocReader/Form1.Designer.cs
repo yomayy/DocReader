54be15ec7c60db93cf4370a17f5fe2974c23ccf1
yomayy <yomaywork@gmail.com>
2019-04-07 17:23:31 +0300

    Sho

---
+++ DocReader/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DocReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 562);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Содержание";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(313, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(940, 562);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Просмотр";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(3, 23);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(307, 536);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(934, 536);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "0.ico");
+            this.imageList1.Images.SetKeyName(1, "1.ico");
+            this.imageList1.Images.SetKeyName(2, "2.ico");
+            this.imageList1.Images.SetKeyName(3, "3.ico");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 562);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Просмоторщик електронных документов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DocReader/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace DocReader
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        XDocument doc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            path = @"..\..\Data\content.xml";
+            doc = XDocument.Load(path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XElement book = doc.Element("book");
+            TreeNode root = new TreeNode()
+            {
+                Text = book.Attribute("name").Value,
+                ImageIndex = Convert.ToInt32(book.Attribute("icon1").Value),
+                SelectedImageIndex = Convert.ToInt32(book.Attribute("icon2").Value),
+            };
+
+            foreach(XElement x in book.Elements("chapter"))
+            {
+                TreeNode item = new TreeNode()
+                {
+                    Text = x.Attribute("name").Value,
+                    ImageIndex = Convert.ToInt32(x.Attribute("icon1").Value),
+                    SelectedImageIndex = Convert.ToInt32(x.Attribute("icon1").Value),
+                };
+
+                foreach(XElement y in x.Elements("page"))
+                {
+                    TreeNode sub = new TreeNode()
+                    {
+                        Text = y.Attribute("id").Value + ".rtf",
+                        ImageIndex = Convert.ToInt32(y.Attribute("icon1").Value),
+                        SelectedImageIndex = Convert.ToInt32(y.Attribute("icon1").Value),
+                    };
+                    item.Nodes.Add(sub);
+                }
+                root.Nodes.Add(item);
+            }
+            treeView1.Nodes.Add(root);
+            treeView1.ExpandAll();
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            string path = @"..\..\Data\";
+            string text = node.Text;
+
+            if (text == "WinForms Guide")
+            {
+                path += "Main.rtf";
+            }
+            else if(text.IndexOf("Chapter") == 0)
+            {
+                string num = text.Split()[1];
+                int k = Convert.ToInt32(num) * 100;
+                path += k.ToString() + ".rtf";
+            }
+            else
+            {
+                path += text;
+            }
+            richTextBox1.LoadFile(path);
+        }
+    }
+}
